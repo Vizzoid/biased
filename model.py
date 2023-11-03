0d43b58f47cd7b36fcd885d4c745cd206a13f1a5
Vizzoid <82728229+Vizzoid@users.noreply.github.com>
2023-11-03 21:42:27 +0000

    Start

--- model.py
+++ model.py
@@ -15,16 +15,7 @@ model = transformers.AutoModelForSequenceClassification.from_pretrained("d4data/
 
 message = "I have a new GPU!"
 tokenized = tokenizer.tokenize(message)
-print(tokenized)
-print(bytes(message, 'utf-8'))
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 3546  # unassigned port
-MESSAGE = "Hello World!"
-
-sock = socket.socket(socket.AF_INET,  # Internet
-                     socket.SOCK_DGRAM)  # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 
 def convert_standard(sentence: string) -> bytes:
