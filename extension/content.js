7f30acb83c3e2772d9def46113c6543c66abcd8f
Vinod Tysoe <82728229+Vizzoid@users.noreply.github.com>
2024-03-05 13:51:53 +0000

    test extension

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,17 @@
 // available variables: document
+
+class TextSearch {
+
+    constructor() {
+        this.text = "";
+    }
+
+    combineText(elements) {
+        for (let i = 0; i < collection.length; i++) {
+            print(elements[i].textContent);
+        }
+    }
+
+}
+
+document.getElementsByTagName("*")
