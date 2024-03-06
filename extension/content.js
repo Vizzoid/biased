27dcdd54ecb7573a14473602f49a7ab2ba5e9fdc
Vinod Tysoe <82728229+Vizzoid@users.noreply.github.com>
2024-03-06 02:41:27 +0000

    text search organization

--- extension/content.js
+++ extension/content.js
@@ -8,9 +8,53 @@ class TextSearch {
 
     combineText(elements) {
         for (let i = 0; i < elements.length; i++) {
-            elements[i].textContent = "warden burger";
+            let element = elements[i];
+            this.combineElement(element);
         }
     }
 
+    /**
+     * Combines the inner text of an element with the interior text collection
+     */
+    combineElement(element) {
+        // account for the element's children
+        if (element.children.length > 0) {
+            this.combineText(element.children);
+            return;
+        }
+        // ignore html elements whose textContent is purposely CSS or JS
+        if (element instanceof HTMLScriptElement || 
+            element instanceof HTMLStyleElement) {
+            return;
+        }
+        // ignore empty text elements
+        if (element.textContent == null) {
+            return;
+        }
+        this.text = this.text + ". \n" + element.textContent;
+    }
+
+    discardExtranousSentences() {
+        let sentences = this.text.replaceAll("…", "").split('.');
+
+        var newText = "";
+        for (let i = 0; i < sentences.length; i++) {
+            let sentence = sentences[i].trim();
+            
+            if (sentence == "" || sentence.length <= 10) {
+                continue;
+            }
+            newText = newText + sentence + ". ";
+        }
+        this.text = newText;
+    }
+
+    debugDisplay(element) {
+        element.textContent = this.text;
+    }
+
 }
-new TextSearch().combineText(document.getElementsByTagName("*"))
+search = new TextSearch();
+search.combineText(document.getElementsByTagName("*"));
+search.discardExtranousSentences();
+search.debugDisplay(document.getElementsByTagName("*")[0]);
